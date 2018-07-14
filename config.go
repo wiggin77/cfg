b0612ac3bf79d1bd79089f4b5fa03a716b7b6c84
wiggin77 <wiggin77@warpmail.net>
2018-07-13 21:15:31 -0400

    create config

---
+++ config.go
@@ -0,0 +1,12 @@
+package config
+
+type Config struct {
+}
+
+func (config *Config) prependSource(cs ConfigSource) {
+
+}
+
+func (config *Config) appendSource(cs ConfigSource) {
+
+}
---
+++ configsource.go
@@ -0,0 +1,4 @@
+package config
+
+type ConfigSource interface {
+}
