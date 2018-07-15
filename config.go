0a55abb9929884122e95cdfe22d936ca25751491
wiggin77 <wiggin77@warpmail.net>
2018-07-15 15:30:39 -0400

    unit tests for config.String & config.Int

--- config.go
+++ config.go
@@ -2,10 +2,11 @@ package config
 
 import (
 	"errors"
+	"strconv"
 	"sync"
 )
 
-var errNotFound = errors.New("not found")
+var ErrNotFound = errors.New("not found")
 
 // Config provides methods for retrieving property values from one or more
 // configuration sources.
@@ -37,23 +38,51 @@ func (config *Config) AppendSource(srcs ...Source) {
 	config.srcs = append(config.srcs, srcs...)
 }
 
-// String returns the value of the named prop as a string,
-// or `ok=false` if the name is not found.
+// String returns the value of the named prop as a string.
+// If the property is not found then the supplied default `def`
+// and `ErrNotFound` are returned.
 //
 // Each `Source` is checked, in the order they are added via
 // `ApppendSource` and `PrependSource`, until a value for the
 // property is found.
-func (config *Config) String(name string) (val string, ok bool) {
+func (config *Config) String(name string, def string) (val string, err error) {
 	config.mutexSrc.RLock()
 	defer config.mutexSrc.RUnlock()
 
+	var ok bool
 	for _, src := range config.srcs {
 		if val, ok = src.GetProp(name); ok {
+			err = nil
 			return
 		}
 	}
+	err = ErrNotFound
+	val = def
 	return
 }
+
+// Int returns the value of the named prop as an `int`.
+// If the property is not found then the supplied default `def`
+// and `ErrNotFound` are returned.
+//
+// See config.String
+func (config *Config) Int(name string, def int) (val int, err error) {
+	var s string
+	if s, err = config.String(name, ""); err == nil {
+		var i int64
+		if i, err = strconv.ParseInt(s, 10, 32); err == nil {
+			val = int(i)
+		}
+	}
+	if err != nil {
+		val = def
+	}
+	return
+}
+
+//
+// Types:  String, Int, Int64, Float64, Bool, milliseconds
+//
 
 // AddChangedListener adds a listener that will receive notifications
 // whenever one or more property values change within the config.
@@ -65,13 +94,13 @@ func (config *Config) AddChangedListener(l ChangedListener) {
 }
 
 // RemoveChangedListener removes all instances of a ChangedListener.
-// Returns non-nil error if the listener was not present.
+// Returns `ErrNotFound` if the listener was not present.
 func (config *Config) RemoveChangedListener(l ChangedListener) error {
 	config.mutexListeners.Lock()
 	defer config.mutexListeners.Unlock()
 
 	dest := make([]ChangedListener, 0, len(config.chgListeners))
-	err := errNotFound
+	err := ErrNotFound
 
 	for _, s := range config.chgListeners {
 		if s != l {
@@ -94,13 +123,13 @@ func (config *Config) AddChangedPropListener(l ChangedPropListener) {
 }
 
 // RemoveChangedPropListener removes all instances of a ChangedPropListener.
-// Returns non-nil error if the listener was not present.
+// Returns `ErrNotFound` if the listener was not present.
 func (config *Config) RemoveChangedPropListener(l ChangedPropListener) error {
 	config.mutexListeners.Lock()
 	defer config.mutexListeners.Unlock()
 
 	dest := make([]ChangedPropListener, 0, len(config.propListeners))
-	err := errNotFound
+	err := ErrNotFound
 
 	for _, s := range config.propListeners {
 		if s != l {
--- config_test.go
+++ config_test.go
@@ -19,11 +19,11 @@ func TestConfig_PrependSource(t *testing.T) {
 	if len(cfg.srcs) != 1 {
 		t.Errorf("Prepend src to empty config; expected len=1, got len=%d", len(cfg.srcs))
 	}
-	if val, ok := cfg.String("prop1"); ok != true || val != "1" {
-		t.Errorf("Prepend src to empty config; expected ok=true, val=1; got ok=%v, val=%s", ok, val)
+	if val, err := cfg.String("prop1", ""); err != nil || val != "1" {
+		t.Errorf("Prepend src to empty config; expected err=nil, val=1; got err=%v, val=%s", err, val)
 	}
-	if _, ok := cfg.String("blap"); ok == true {
-		t.Errorf("Prepend src to empty config; expected ok=false for missing prop, got ok=%v", ok)
+	if _, err := cfg.String("blap", ""); err == nil {
+		t.Errorf("Prepend src to empty config; expected err=false for missing prop, got err=%v", err)
 	}
 
 	// Prepend second
@@ -31,11 +31,11 @@ func TestConfig_PrependSource(t *testing.T) {
 	if len(cfg.srcs) != 2 {
 		t.Errorf("Prepend second src; expected len=2, got len=%d", len(cfg.srcs))
 	}
-	if val, ok := cfg.String("prop1"); ok != true || val != "2" {
-		t.Errorf("Prepend second src; for prop1 expected ok=true, val=2; got ok=%v, val=%s", ok, val)
+	if val, err := cfg.String("prop1", ""); err != nil || val != "2" {
+		t.Errorf("Prepend second src; for prop1 expected err=nil, val=2; got err=%v, val=%s", err, val)
 	}
-	if val, ok := cfg.String("prop2"); ok != true || val != "2" {
-		t.Errorf("Prepend second src; for prop2 expected ok=true, val=2; got ok=%v, val=%s", ok, val)
+	if val, err := cfg.String("prop2", ""); err != nil || val != "2" {
+		t.Errorf("Prepend second src; for prop2 expected err=nil, val=2; got err=%v, val=%s", err, val)
 	}
 
 	// Prepend third
@@ -43,17 +43,17 @@ func TestConfig_PrependSource(t *testing.T) {
 	if len(cfg.srcs) != 3 {
 		t.Errorf("Prepend third src; expected len=3, got len=%d", len(cfg.srcs))
 	}
-	if val, ok := cfg.String("prop1"); ok != true || val != "3" {
-		t.Errorf("Prepend third src; for prop1 expected ok=true, val=3; got ok=%v, val=%s", ok, val)
+	if val, err := cfg.String("prop1", ""); err != nil || val != "3" {
+		t.Errorf("Prepend third src; for prop1 expected err=nil, val=3; got err=%v, val=%s", err, val)
 	}
-	if val, ok := cfg.String("prop2"); ok != true || val != "3" {
-		t.Errorf("Prepend third src; for prop2 expected ok=true, val=3; got ok=%v, val=%s", ok, val)
+	if val, err := cfg.String("prop2", ""); err != nil || val != "3" {
+		t.Errorf("Prepend third src; for prop2 expected err=nil, val=3; got err=%v, val=%s", err, val)
 	}
-	if val, ok := cfg.String("prop3"); ok != true || val != "3" {
-		t.Errorf("Prepend third src; for prop3 expected ok=true, val=3; got ok=%v, val=%s", ok, val)
+	if val, err := cfg.String("prop3", ""); err != nil || val != "3" {
+		t.Errorf("Prepend third src; for prop3 expected err=nil, val=3; got err=%v, val=%s", err, val)
 	}
-	if _, ok := cfg.String("blap"); ok == true {
-		t.Errorf("Prepend third src; expected ok=false for missing prop, got ok=%v", ok)
+	if _, err := cfg.String("blap", ""); err == nil {
+		t.Errorf("Prepend third src; expected err=false for missing prop, got err=%v", err)
 	}
 }
 
@@ -72,11 +72,11 @@ func TestConfig_AppendSource(t *testing.T) {
 	if len(cfg.srcs) != 1 {
 		t.Errorf("Append src to empty config; expected len=1, got len=%d", len(cfg.srcs))
 	}
-	if val, ok := cfg.String("prop1"); ok != true || val != "1" {
-		t.Errorf("Append src to empty config; expected ok=true, val=1; got ok=%v, val=%s", ok, val)
+	if val, err := cfg.String("prop1", ""); err != nil || val != "1" {
+		t.Errorf("Append src to empty config; expected err=nil, val=1; got err=%v, val=%s", err, val)
 	}
-	if _, ok := cfg.String("blap"); ok == true {
-		t.Errorf("Append src to empty config; expected ok=false for missing prop, got ok=%v", ok)
+	if _, err := cfg.String("blap", ""); err == nil {
+		t.Errorf("Append src to empty config; expected err=false for missing prop, got err=%v", err)
 	}
 
 	// Append second
@@ -84,11 +84,11 @@ func TestConfig_AppendSource(t *testing.T) {
 	if len(cfg.srcs) != 2 {
 		t.Errorf("Append second src; expected len=2, got len=%d", len(cfg.srcs))
 	}
-	if val, ok := cfg.String("prop1"); ok != true || val != "1" {
-		t.Errorf("Append second src; for prop1 expected ok=true, val=1; got ok=%v, val=%s", ok, val)
+	if val, err := cfg.String("prop1", ""); err != nil || val != "1" {
+		t.Errorf("Append second src; for prop1 expected err=nil, val=1; got err=%v, val=%s", err, val)
 	}
-	if val, ok := cfg.String("prop2"); ok != true || val != "2" {
-		t.Errorf("Append second src; for prop2 expected ok=true, val=2; got ok=%v, val=%s", ok, val)
+	if val, err := cfg.String("prop2", ""); err != nil || val != "2" {
+		t.Errorf("Append second src; for prop2 expected err=nil, val=2; got err=%v, val=%s", err, val)
 	}
 
 	// Append third
@@ -96,21 +96,21 @@ func TestConfig_AppendSource(t *testing.T) {
 	if len(cfg.srcs) != 3 {
 		t.Errorf("Append third src; expected len=3, got len=%d", len(cfg.srcs))
 	}
-	if val, ok := cfg.String("prop1"); ok != true || val != "1" {
-		t.Errorf("Append third src; for prop1 expected ok=true, val=1; got ok=%v, val=%s", ok, val)
+	if val, err := cfg.String("prop1", ""); err != nil || val != "1" {
+		t.Errorf("Append third src; for prop1 expected err=nil, val=1; got err=%v, val=%s", err, val)
 	}
-	if val, ok := cfg.String("prop2"); ok != true || val != "2" {
-		t.Errorf("Append third src; for prop2 expected ok=true, val=2; got ok=%v, val=%s", ok, val)
+	if val, err := cfg.String("prop2", ""); err != nil || val != "2" {
+		t.Errorf("Append third src; for prop2 expected err=nil, val=2; got err=%v, val=%s", err, val)
 	}
-	if val, ok := cfg.String("prop3"); ok != true || val != "3" {
-		t.Errorf("Append third src; for prop3 expected ok=true, val=3; got ok=%v, val=%s", ok, val)
+	if val, err := cfg.String("prop3", ""); err != nil || val != "3" {
+		t.Errorf("Append third src; for prop3 expected err=nil, val=3; got err=%v, val=%s", err, val)
 	}
-	if _, ok := cfg.String("blap"); ok == true {
-		t.Errorf("Append third src; expected ok=false for missing prop, got ok=%v", ok)
+	if _, err := cfg.String("blap", ""); err == nil {
+		t.Errorf("Append third src; expected err=false for missing prop, got err=%v", err)
 	}
 }
 
-func TestConfig_AddChangedListener(t *testing.T) {
+func TestConfig_AddRemoveChangedListener(t *testing.T) {
 	map1 := map[string]string{"prop1": "1"}
 	src1 := NewSrcMapFromMap(map1)
 	cfg := &Config{}
@@ -150,6 +150,47 @@ func TestConfig_AddChangedListener(t *testing.T) {
 	}
 }
 
+func TestConfig_AddRemoveChangedPropListener(t *testing.T) {
+	map1 := map[string]string{"prop1": "1"}
+	src1 := NewSrcMapFromMap(map1)
+	cfg := &Config{}
+	cfg.AppendSource(src1)
+
+	tl1 := &TestListener{}
+	tl2 := &TestListener{}
+	tl3 := &TestListener{}
+
+	// Test Add
+	cfg.AddChangedPropListener(tl1)
+	if len(cfg.propListeners) != 1 {
+		t.Errorf("AddChangedPropListener; expected len=1, got len=%d", len(cfg.propListeners))
+	}
+	cfg.AddChangedPropListener(tl2)
+	if len(cfg.propListeners) != 2 {
+		t.Errorf("AddChangedPropListener; expected len=2, got len=%d", len(cfg.propListeners))
+	}
+	cfg.AddChangedPropListener(tl3)
+	cfg.AddChangedPropListener(tl1)
+	if len(cfg.propListeners) != 4 {
+		t.Errorf("AddChangedPropListener; expected len=4, got len=%d", len(cfg.propListeners))
+	}
+
+	// Test remove
+	cfg.RemoveChangedPropListener(tl1)
+	if len(cfg.propListeners) != 2 {
+		t.Errorf("RemoveChangedPropListener; expected len=2, got len=%d", len(cfg.propListeners))
+	}
+	cfg.RemoveChangedPropListener(tl2)
+	if len(cfg.propListeners) != 1 {
+		t.Errorf("RemoveChangedPropListener; expected len=1, got len=%d", len(cfg.propListeners))
+	}
+	cfg.RemoveChangedPropListener(tl3)
+	if len(cfg.propListeners) != 0 {
+		t.Errorf("RemoveChangedPropListener; expected len=0, got len=%d", len(cfg.propListeners))
+	}
+}
+
+// ChangeListener and ChangedPropListener
 type TestListener struct {
 	_ [1]byte // cannot be empty struct
 }
@@ -158,4 +199,76 @@ func (l *TestListener) Changed(cfg *Config, src *Source) {
 }
 
 func (l *TestListener) ChangedProp(cfg *Config, src *Source, name string) {
+}
+
+func TestConfig_String(t *testing.T) {
+	src1 := NewSrcMapFromMap(map[string]string{"prop1": "1"})
+	src2 := NewSrcMapFromMap(map[string]string{"prop2": "2", "prop1": "2"})
+	src3 := NewSrcMapFromMap(map[string]string{"prop3": "3", "prop2": "3", "prop1": "3"})
+	config := &Config{}
+	config.AppendSource(src1, src2, src3)
+	type args struct {
+		prop string
+		def  string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantVal string
+		wantErr error
+	}{
+		{"Missing prop", args{"blap", "x"}, "x", ErrNotFound},
+		{"Prop1", args{"prop1", "x"}, "1", nil},
+		{"Prop2", args{"prop2", "x"}, "2", nil},
+		{"Prop3", args{"prop3", "x"}, "3", nil},
+		{"Blank prop", args{"", "x"}, "x", ErrNotFound},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotVal, gotErr := config.String(tt.args.prop, tt.args.def)
+			if gotVal != tt.wantVal {
+				t.Errorf("Config.String() gotVal = %v, want %v", gotVal, tt.wantVal)
+			}
+			if gotErr != tt.wantErr {
+				t.Errorf("Config.String() gotErr = %v, want %v", gotErr, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestConfig_Int(t *testing.T) {
+	src1 := NewSrcMapFromMap(map[string]string{"prop1": "-1", "big": "2147483647"})
+	src2 := NewSrcMapFromMap(map[string]string{"prop2": "2", "prop1": "2", "zero": "0"})
+	src3 := NewSrcMapFromMap(map[string]string{"prop3": "3", "prop2": "3", "prop1": "3"})
+	config := &Config{}
+	config.AppendSource(src1, src2, src3)
+	type args struct {
+		prop string
+		def  int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantVal int
+		wantErr error
+	}{
+		{"Missing prop", args{"blap", 77}, 77, ErrNotFound},
+		{"Prop1", args{"prop1", 77}, -1, nil},
+		{"Prop2", args{"prop2", 77}, 2, nil},
+		{"Prop3", args{"prop3", 77}, 3, nil},
+		{"zero", args{"zero", 77}, 0, nil},
+		{"big", args{"big", 77}, 2147483647, nil},
+		{"Blank prop", args{"", 77}, 77, ErrNotFound},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotVal, gotErr := config.Int(tt.args.prop, tt.args.def)
+			if gotVal != tt.wantVal {
+				t.Errorf("Config.Int() gotVal = %v, want %v", gotVal, tt.wantVal)
+			}
+			if gotErr != tt.wantErr {
+				t.Errorf("Config.Int() gotErr = %v, want %v", gotErr, tt.wantErr)
+			}
+		})
+	}
 }
